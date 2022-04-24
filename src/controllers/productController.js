e643667bc059b78449ac7248721f44746baa7c59
JeanetteBarrera <jeanette.barrera0710@gmail.com>
2022-04-24 17:51:26 -0300

    product CRUD

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,9 +1,95 @@
+const Product = require("../models/Product");
+
 let controller = {
-    list: (req, res) => {
-        res.send("LISTADO DE PRODUCTOS")
+    create: async (req, res) => {
+        
+        const newProduct = new Product(req.body);
+
+        try {
+            
+            const saveProduct = await newProduct.save();
+            res.status(200).json(saveProduct);
+
+        } catch (err) {
+            
+            res.status(500).json(err)
+        }
     },
-    search: (req, res) => {
-        res.send("RESULTADO DE BUSQUEDA")
-    }
+    updateProduct: async (req, res) => {
+       
+        try {
+
+            const updateProduct  = await Product.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set : req.body
+                },
+                {
+                    new : true
+                }
+            );
+            res.status(200).json(updateProduct);
+            
+        } catch (err) {
+            
+            res.status(500).json(err)
+        }
+    },
+    deleteProduct: async (req, res) => {
+
+        try {
+            
+            const product = await Product.findByIdAndDelete(req.params.id);
+            res.status(200).json("Product has been deleted...")
+
+        } catch (err) {
+            
+            res.status(500).json(err)
+        }
+
+    },
+    findProduct: async (req, res) => {
+
+        try {
+            
+            const product = await Product.findById(req.params.id);
+            res.status(200).json(product);
+
+        } catch (err) {
+            
+            res.status(500).json(err)
+        }
+    },
+    allProducts: async (req, res) => {
+        const news = req.query.new;
+        const category = req.query.category;
+
+        try {
+
+            let products;
+            
+            if(news) {
+
+                products = await Product.find().sort({ createdAt : -1 });
+            } else if(category) {
+
+                products = await Product.find({
+                    categories : {
+                        $in : [category]
+                    }
+                })
+            } else {
+
+                products = await Product.find();
+            }
+
+            res.status(200).json(products)
+
+        } catch (err) {
+            
+            res.status(500).json(err)
+        }
+    }   
 }
-export default controller;
+
+module.exports = controller;
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,14 +1,13 @@
-import { Router } from "express";
-import controller from "../controllers/productController";
-
-const router = Router();
-
-
-router.get("/", controller.list);
-router.get("/search", controller.search);
+const router = require("express").Router();
+const controller = require("../controllers/productController");
+const { verifyTokenAndAdmin } = require("../middlewares/verifyToken")
 
 
+router.post("/create", verifyTokenAndAdmin, controller.create);
+router.put("/:id", verifyTokenAndAdmin, controller.updateProduct);
+router.delete("/:id", verifyTokenAndAdmin, controller.deleteProduct);
+router.get("/:id", controller.findProduct);
+router.get("/", controller.allProducts);
 
 
-
-export default router;
+module.exports = router;
